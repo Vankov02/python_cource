d0438ec517239b0112cffdeaaf2369370a93b673
Ivan Maslennikov <masliennikov.2002@mail.ru>
2023-10-28 17:50:29 +0700

    Сделана программа "Возраст"!

---
+++ Lesson 3/age.py
@@ -0,0 +1,9 @@
+user_age = int(input("Введите ваш возраст: "))
+
+if ((user_age % 100 > 10) and (user_age % 100 < 15)) or (user_age % 10 == 0) or (
+        (user_age % 10 > 4) and (user_age % 10 <= 9)):
+    print(f"Вам {user_age} лет!")
+elif user_age % 10 == 1:
+    print(f"Вам {user_age} год!")
+else:
+    print(f"Вам {user_age} года!")
