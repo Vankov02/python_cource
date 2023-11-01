a5431521acc66a2ed93fd4cd7dba6ac644fea310
Ivan Maslennikov <masliennikov.2002@mail.ru>
2023-11-02 00:37:53 +0700

    Исправлены замечания в задаче "Возраст"

--- Lesson 3/age.py
+++ Lesson 3/age.py
@@ -1,13 +1,14 @@
-user_age = int(input("Введите ваш возраст: "))
+age = int(input("Введите ваш возраст: "))
+age_per_100_multiple = age % 100
+age_per_10_multiple = age % 10
 
-if user_age > 112:
+if age > 112:
     print("Вы слишком стары!")
-elif user_age < 1:
+elif age < 1:
     print("Вы слишком молоды!")
-elif ((user_age % 100 > 10) and (user_age % 100 < 15)) or (user_age % 10 == 0) or (
-        (user_age % 10 > 4) and (user_age % 10 <= 9)):
-    print(f"Вам {user_age} лет!")
-elif user_age % 10 == 1:
-    print(f"Вам {user_age} год!")
+elif 10 <= age_per_100_multiple < 15 or age_per_10_multiple == 0 or 4 < age_per_10_multiple <= 9:
+    print(f"Вам {age} лет!")
+elif age % 10 == 1:
+    print(f"Вам {age} год!")
 else:
-    print(f"Вам {user_age} года!")
+    print(f"Вам {age} года!")
