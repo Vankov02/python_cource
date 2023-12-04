a1682400398c899c88043a21714fa8b5160ea80d
Ivan Maslennikov <masliennikov.2002@mail.ru>
2023-12-04 07:35:15 +0700

    Сделаны задачи: 10 чисел в строке

--- Lesson 3/quadratic_equation.py
+++ Lesson 3/quadratic_equation.py
@@ -6,28 +6,20 @@ a = float(input("Введите коэффициент при x^2: "))
 b = float(input("Введите коэффициент при x: "))
 c = float(input("Введите свободный член: "))
 
-if (abs(a - 0) < EPSILON and abs(b - 0) < EPSILON and
-        abs(c - 0) < EPSILON or a - 0 > EPSILON and b - 0 > EPSILON and c - 0 > EPSILON):
-    print("x принадлежит R")
-elif abs(a - 0) < EPSILON and abs(b - 0) < EPSILON:
-    print("Решение данного уравнения: пустое множество")
-elif abs(a - 0) < EPSILON and abs(c - 0) < EPSILON or abs(b - 0) < EPSILON and abs(c - 0) < EPSILON:
-    print("Решение данного уравнения: x = 0")
-elif abs(a - 0) < EPSILON:
-    result = -c / b
-    print(f"Решение данного уравнения: x = {result}")
+if abs(a - 0) < EPSILON:
+    if abs(b - 0) < EPSILON:
+        if abs(c - 0) < EPSILON:
+            print("0 = 0")
+        else:
+            print("Корней нет!")
+    else:
+        print(f"Решение данного уравнения: x = {-c / b:.3f}")
 else:
     discriminant = b ** 2 - 4 * a * c
-    result_1 = (-b + math.sqrt(discriminant)) / (2 * a)
-    result_2 = (-b - math.sqrt(discriminant)) / (2 * a)
-    print(f"Решение данного уравнения: x_1 = {result_1}, x_2 = {result_2}")
 
-# elif 0 - discriminant > EPSILON:
-#     print("Корней решения данного уравнения нет!")
-# elif abs(discriminant - 0) < EPSILON:
-#     result_1 = -b / (2 * a)
-#     print(f"Корень решения данного уравнения: {result_1}")
-# else:
-#     result_1 = (-b + math.sqrt(discriminant)) / (2 * a)
-#     result_2 = (-b - math.sqrt(discriminant)) / (2 * a)
-#     print(f"Корни решения данного уравнения: {result_1} и {result_2}")
+    if 0 - discriminant > EPSILON:
+        print("Корней нет!")
+    else:
+        result_1 = (-b + math.sqrt(discriminant)) / (2 * a)
+        result_2 = (-b - math.sqrt(discriminant)) / (2 * a)
+        print(f"Решение данного уравнения: x_1 = {result_1:.3f}, x_2 = {result_2:.3f}")
